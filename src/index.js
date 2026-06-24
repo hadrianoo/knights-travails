20e3892a8e84f4d47d35947602559ebb17437e67
hadriano <57418210+hadrianoo@users.noreply.github.com>
2026-06-24 12:16:55 +0200

    wip: generate all possible moves for knight position

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ class Node {
     this.knightPosition = knightPosition;
     this.possibleMovesList = [];
   }
-  addMove(newPosition) {
-    this.possibleMovesList.push(newPosition);
+  #addMove(posX, posY) {
+    if (posX >= 0 && posY >= 0 && posX <= 7 && posY <= 7)
+      this.possibleMovesList.push([posX, posY]);
+  }
+  generatePossibleMoves() {
+    const [x, y] = this.knightPosition;
+    this.#addMove(x + 1, y + 2);
+    this.#addMove(x + 2, y + 1);
+    this.#addMove(x + 2, y - 1);
+    this.#addMove(x + 1, y - 2);
+    this.#addMove(x - 1, y - 2);
+    this.#addMove(x - 2, y - 1);
+    this.#addMove(x - 2, y + 1);
+    this.#addMove(x - 1, y + 2);
   }
 }
 
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,8 +1,36 @@
 import { Node, knightMoves } from "./index.js";
 describe("test Node class", () => {
-  const knightPos = new Node([0, 0]);
-  test("add moves to possibleMovesList", () => {
-    knightPos.addMove([1, 0]);
-    expect(knightPos.possibleMovesList).toEqual([[1, 0]]);
+  test("populate possibleMovesList with all possible moves", () => {
+    const knightPos = new Node([3, 3]);
+    knightPos.generatePossibleMoves();
+    expect(knightPos.possibleMovesList).toEqual([
+      [4, 5],
+      [5, 4],
+      [5, 2],
+      [4, 1],
+      [2, 1],
+      [1, 2],
+      [1, 4],
+      [2, 5],
+    ]);
+  });
+  test("populate possibleMovesList with all possible moves, allow only positive values", () => {
+    const knightPos = new Node([0, 0]);
+    knightPos.generatePossibleMoves();
+    expect(knightPos.possibleMovesList).toEqual([
+      [1, 2],
+      [2, 1],
+    ]);
+  });
+  test("populate possibleMovesList with all possible moves, allow only values <= 7", () => {
+    const knightPos = new Node([6, 6]);
+    knightPos.generatePossibleMoves();
+    console.log(knightPos.possibleMovesList);
+    expect(knightPos.possibleMovesList).toEqual([
+      [7, 4],
+      [5, 4],
+      [4, 5],
+      [4, 7],
+    ]);
   });
 });
